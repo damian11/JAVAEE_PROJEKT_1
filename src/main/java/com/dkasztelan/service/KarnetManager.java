8c95b66b6d63febed92411272004ba19cf6604b4
damian11 <damian.kasztelan1@op.pl>
2015-11-17 23:37:17 +0100

    uzupelnianie klasy KarnetManager.java

--- src/main/java/com/dkasztelan/service/KarnetManager.java
+++ src/main/java/com/dkasztelan/service/KarnetManager.java
@@ -3,5 +3,11 @@ package main.java.com.dkasztelan.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import main.java.com.dkasztelan.domain.Klient;
-import main.java.com.dkasztelan.domain.SprzedazKarnetu;
+import main.java.com.dkasztelan.domain.Karnet;
+
+public class KarnetManager {
+	
+	private List<Karnet> karnety = new ArrayList<Karnet>();
+	private int id_karnet = 0;
+
+}
