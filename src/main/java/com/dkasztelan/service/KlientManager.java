5c710cceff15206232d943f813aec4710d0c6261
damian11 <damian.kasztelan1@op.pl>
2015-11-18 00:33:26 +0100

    dodanie metody dodajKlienta do KlientManager.java

--- src/main/java/com/dkasztelan/service/KlientManager.java
+++ src/main/java/com/dkasztelan/service/KlientManager.java
@@ -7,5 +7,16 @@ import main.java.com.dkasztelan.domain.Klient;
 
 public class KlientManager {
 	
+	private List<Klient> klienci = new ArrayList<Klient>();
+	private int id_klient = 0;
+
+	public void dodajKlienta(Klient klient){
+		Klient nowyKlient = new Klient(klient.getImie(), klient.getNazwisko(), klient.getTelefon());
+	id_klient++;
+	nowyKlient.setId_klient(id_klient);
+	klienci.add(nowyKlient);
+}
+	
+	
 	
 }
