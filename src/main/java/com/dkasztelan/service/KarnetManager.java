dafd57956b86b76ac9ff8ba3f09e13c9be1e519a
damian11 <damian.kasztelan1@op.pl>
2015-11-17 22:43:22 +0100

    dodanie klasy KarnetManager.java

---
+++ src/main/java/com/dkasztelan/service/KarnetManager.java
@@ -0,0 +1,7 @@
+package main.java.com.dkasztelan.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.com.dkasztelan.domain.Klient;
+import main.java.com.dkasztelan.domain.SprzedazKarnetu;
