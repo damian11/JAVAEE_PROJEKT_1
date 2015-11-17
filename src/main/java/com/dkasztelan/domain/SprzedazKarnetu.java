35fedd13e2f62825e7979158ec461a46da6278e6
damian11 <damian.kasztelan1@op.pl>
2015-11-17 21:21:21 +0100

    dodanie klasy SprzedazKarnetu.java

---
+++ src/main/java/com/dkasztelan/domain/SprzedazKarnetu.java
@@ -0,0 +1,31 @@
+package main.java.com.dkasztelan.domain;
+
+
+public class SprzedazKarnetu {
+
+	private int id_sprzedazKarnetu;
+	private int id_klient;
+	private int id_karnet;
+	
+	
+	
+	
+	public SprzedazKarnetu(int id_klient, int id_karnet){
+	
+	}
+
+
+	public SprzedazKarnetu() {
+			}
+
+
+
+
+
+
+	}
+	
+	
+
+
+
