a96f1bc317a9f26ea647dc5f2a543533d21d8232
damian11 <damian.kasztelan1@op.pl>
2015-11-18 00:24:19 +0100

    dodanie klasy KlientManager.java

---
+++ src/main/java/com/dkasztelan/service/KlientManager.java
@@ -0,0 +1,11 @@
+package main.java.com.dkasztelan.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.com.dkasztelan.domain.Klient;
+
+public class KlientManager {
+	
+	
+}
