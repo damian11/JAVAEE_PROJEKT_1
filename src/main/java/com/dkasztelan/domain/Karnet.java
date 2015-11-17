16328323baf51847101ed541a98a24010f87cdee
damian11 <damian.kasztelan1@op.pl>
2015-11-17 20:54:40 +0100

    uzupełnienie klasy Karnet.java

--- src/main/java/com/dkasztelan/domain/Karnet.java
+++ src/main/java/com/dkasztelan/domain/Karnet.java
@@ -21,5 +21,38 @@ public class Karnet {
 		this.cena = cena;
 		
 	}
+
+	public int getId_karnet(){
+		return id_karnet;
+	}
+	
+	public void setId_karnet(int id_karnet){
+		this.id_karnet = id_karnet;
+	}
+	
+	public String getRodzaj(){
+		return rodzaj;
+	}
+	public void setRodzaj(String rodzaj){
+		this.rodzaj = rodzaj;
+		
+	}
+	
+	public String getOpis(){
+		return opis;
+	}
+	
+	public void setOpis(String opis){
+		this.opis = opis;
+	}
+	
+	public double getCena(){
+		return cena;
+	}
+	
+	public void setCena(double cena){
+		this.cena = cena;
+	
+	}
 	
 	}
