505678c5f229b0cd1ff222294eb9719373868e45
damian11 <damian.kasztelan1@op.pl>
2015-11-18 01:14:57 +0100

    dodanie metod dodajKarnet i dodajKlienta do klasy SprzedazKarnetuManager.java

--- src/main/java/com/dkasztelan/service/SprzedazKarnetuManager.java
+++ src/main/java/com/dkasztelan/service/SprzedazKarnetuManager.java
@@ -7,13 +7,28 @@ import main.java.com.dkasztelan.domain.Klient;
 import main.java.com.dkasztelan.domain.Karnet;
 
 public class SprzedazKarnetuManager {
-
+	
+	private List<SprzedazKarnetu> sprzedazKarnetu = new ArrayList<SprzedazKarnetu>();
 	private List<Klient> klienci = new ArrayList<Klient>();
 	private List<Karnet> karnety = new ArrayList<Karnet>();
+
+	private int id_sprzedazKarnetu = 0;
 	private int id_klient = 0;
 	private int id_karnet = 0;
 	
 
-
+	public void dodajKlienta(Klient klient){
+	Klient nowyKlient = new Klient(klient.getImie(), klient.getNazwisko(), klient.getTelefon());
+	id_klient++;
+	nowyKlient.setId_klient(id_klient);
+	klienci.add(nowyKlient);
+	}
+	
+	public void dodajKarnet(Karnet karnet){
+	Karnet nowyKarnet = new Karnet(karnet.getRodzaj(), karnet.getOpis(), karnet.getCena());
+	id_karnet++;
+	nowyKarnet.setId_karnet(id_karnet);
+	karnety.add(nowyKarnet);
+	}
 	
 }
