935e65fc54e9173adf82b6602f2f3b654ea3366c
damian11 <damian.kasztelan1@op.pl>
2015-11-18 01:03:06 +0100

    dodanie klasy SprzedazKarnetuManager

---
+++ src/main/java/com/dkasztelan/service/SprzedazKarnetuManager.java
@@ -0,0 +1,19 @@
+package main.java.com.dkasztelan.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.com.dkasztelan.domain.Klient;
+import main.java.com.dkasztelan.domain.Karnet;
+
+public class SprzedazKarnetuManager {
+
+	private List<Klient> klienci = new ArrayList<Klient>();
+	private List<Karnet> karnety = new ArrayList<Karnet>();
+	private int id_klient = 0;
+	private int id_karnet = 0;
+	
+
+
+	
+}
